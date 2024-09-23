60ed53f22c94101f19fc90f1a1754429a283eb3e
SDM0 <60699143+SDM0@users.noreply.github.com>
2024-09-23 13:21:56 +0200

    Wrong version bump

--- main.lua
+++ main.lua
@@ -7,7 +7,7 @@
 --- BADGE_COLOUR: 18cadc
 --- DISPLAY_NAME: Joker Evolution
 --- PREFIX: evo
---- VERSION: 1.1.0b
+--- VERSION: 1.1.0c
 --- LOADER_VERSION_GEQ: 1.0.0 
 
 ----------------------------------------------
