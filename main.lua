db17aac40584d5adadbf397ee882c78e1b2afdf5
SDM0 <60699143+SDM0@users.noreply.github.com>
2024-09-26 00:55:02 +0200

    Version bump

--- main.lua
+++ main.lua
@@ -7,7 +7,7 @@
 --- BADGE_COLOUR: 18cadc
 --- DISPLAY_NAME: Joker Evolution
 --- PREFIX: evo
---- VERSION: 1.1.0c
+--- VERSION: 1.1.1a
 --- LOADER_VERSION_GEQ: 1.0.0 
 
 ----------------------------------------------
