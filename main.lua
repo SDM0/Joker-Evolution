5eb73b8f2225c83d744792db11843e1495e50dd0
SDM0 <60699143+SDM0@users.noreply.github.com>
2024-11-22 17:11:21 +0100

    Version bump and localization/config changes

--- main.lua
+++ main.lua
@@ -7,7 +7,7 @@
 --- BADGE_COLOUR: 18cadc
 --- DISPLAY_NAME: Joker Evolution
 --- PREFIX: evo
---- VERSION: 1.2.0a
+--- VERSION: 1.2.1a
 --- DEPENDENCIES: [Steamodded>=1.0.0~ALPHA-1030f]
 
 ----------------------------------------------
@@ -19,31 +19,9 @@ JokerEvolution_Config = JokerEvolution_Mod.config
 JokerEvolution = {}
 JokerEvolution.evolutions = {}
 
-function JokerEvolution_Mod.process_loc_text()
-	G.localization.misc.dictionary["k_evolution_ex"] = "Evolution!"
-	G.localization.misc.dictionary["b_evolve"] = "EVOLVE"
-    G.localization.descriptions.Other.modified_card = {
-        name = "Modified",
-        text = {
-            "Enhancement, seal, edition"
-        }
-    }
-    G.localization.descriptions.Other.luchador = {  -- Recreating Luchador tooltip to avoid showing evo tooltip on Superstar
-        name = "Luchador",
-        text = {
-            "Sell this card to",
-            "disable the current",
-            "{C:attention}Boss Blind{}",
-        }
-    }
-end
-
 SMODS.Rarity{
     key = "evo",
     prefix_config = {key = false},
-    loc_txt = {
-        name = "Evolved"
-    },
     badge_colour = G.C.GOLD,
     pools = {["Joker"] = true},
     get_weight = function(self, weight, object_type)
@@ -51,13 +29,15 @@ SMODS.Rarity{
     end,
 }
 
-SMODS.load_file("localization.lua")()
 SMODS.load_file("overrides.lua")()
 SMODS.load_file("functions.lua")()
 
 if JokerEvolution_Config.enable_jokers then
     SMODS.load_file("data/example_joker.lua")()
     SMODS.load_file("data/jokers.lua")()
+    if JokerDisplay then
+        SMODS.load_file("jokerdisplay_definitions.lua")()
+    end
 end
 
 if JokerEvolution_Config.enable_consus then
@@ -92,6 +72,15 @@ JokerEvolution_Mod.config_tab = function()
             }},
             {n = G.UIT.C, config = { align = "c", padding = 0 }, nodes = {
                 { n = G.UIT.T, config = { text = "Enable Extra Jokers (ex: Collector Joker)", scale = 0.42, colour = G.C.UI.TEXT_LIGHT }},
+            }},
+        }},
+
+        {n = G.UIT.R, config = {align = "cl", padding = 0}, nodes = {
+            {n = G.UIT.C, config = { align = "cl", padding = 0.05 }, nodes = {
+                create_toggle{ col = true, label = "", scale = 1, w = 0, shadow = true, ref_table = JokerEvolution_Config, ref_value = "enable_pool_restriction" },
+            }},
+            {n = G.UIT.C, config = { align = "c", padding = 0 }, nodes = {
+                { n = G.UIT.T, config = { text = "Enable poll restriction", scale = 0.42, colour = G.C.UI.TEXT_LIGHT }},
             }},
         }},
 
