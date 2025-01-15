ee0d1aa816427c49ab126804cb7bdc707abcdb50
SDM0 <60699143+SDM0@users.noreply.github.com>
2025-01-15 11:23:47 +0100

    Moved "Evolution_tooltip_object" to an override

--- overrides.lua
+++ overrides.lua
@@ -5,7 +5,7 @@ local use_and_sell_buttonsref = G.UIDEF.use_and_sell_buttons
 function G.UIDEF.use_and_sell_buttons(card)
 	local retval = use_and_sell_buttonsref(card)
 	if card.area and card.area.config.type == 'joker' and card.ability.set == 'Joker' and card:get_card_evolution() then
-		local evolve = 
+		local evolve =
 		{n=G.UIT.C, config={h=0.6, align = "cr"}, nodes={
 		  {n=G.UIT.C, config={ref_table = card, align = "cr",maxw = 1.25, padding = 0.1, r=0.08, minw = 1.25, hover = true, shadow = true, colour = G.C.GOLD, one_press = true, button = 'sell_card', func = 'can_evolve_card'}, nodes={
 			{n=G.UIT.C, config={align = "tm"}, nodes={
@@ -37,8 +37,8 @@ function Card:update(dt)
 
 				self.ability.evolution.jiggle = true
 			end
-		
-			if not self:can_evolve_card() and self.ability.evolution.jiggle then 
+
+			if not self:can_evolve_card() and self.ability.evolution.jiggle then
 				self.ability.evolution.jiggle = false
 			end
 		end
@@ -52,6 +52,12 @@ function Card:calculate_joker(context)
 		self:calculate_evo(context)
 	end
 	return ret
+end
+
+local generate_UIBox_ability_tableref = Card.generate_UIBox_ability_table
+function Card:generate_UIBox_ability_table(vars_only)
+	Evolution_tooltip_object = self
+	return generate_UIBox_ability_tableref(self, vars_only)
 end
 
 local level_up_handref = level_up_hand
