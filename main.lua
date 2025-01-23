f19c49cfbb651c970c4140d95b2ad5562f3be1ce
SDM0 <60699143+SDM0@users.noreply.github.com>
2025-01-23 11:38:58 +0100

    Version bump

--- main.lua
+++ main.lua
@@ -7,7 +7,7 @@
 --- BADGE_COLOUR: 18cadc
 --- DISPLAY_NAME: Joker Evolution
 --- PREFIX: evo
---- VERSION: 1.2.1c
+--- VERSION: 1.2.1d
 --- DEPENDENCIES: [Steamodded>=1.0.0~ALPHA-1030f]
 
 ----------------------------------------------
